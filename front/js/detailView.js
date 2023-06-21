dcb7f96e5ecfe00857306adcdb4dce20d7890aae
chris_beltran <cristianb72@hotmail.com>
2023-06-20 18:34:46 -0600

    detailView

--- front/js/detailView.js
+++ front/js/detailView.js
@@ -1,25 +1,33 @@
 
-///crear la fetch para que te traiga a la card
-/**
- * 
- * fetch(`${baseURL}/posts`)
-  .then(res => res.json())
-  .then(data => {
- */
-
+const baseURL = "http://localhost:8800";
+const getMytoken = localStorage.getItem("token");
+let params = new URLSearchParams(document.location.search)
+let postCardId = params.get("postCardId");
+console.log(postCardId);
+const getCardInfo = async(postCardId) => {
+  let response = await fetch(`${baseURL}/posts/${postCardId}`,{
+    method:'GET',
+    headers: { "Content-Type": "application/json", "Authorization":`Bearer ${getMytoken}`},
+    
+  }); 
+  let data= await response.json();
+  console.log(data);
+  return data,data
+       
+}
 
 const printCard = async() => {
     let CardInfo = await getCardInfo(postCardId)
     let html = `<div class="card mb-3" style="max-width: 540px;">
     <div class="row g-0">
       <div class="col-md-12" style="align-item: center;">
-        <img src=${CardInfo.picture} class="img-fluid rounded-start" style="height:100%" alt="...">
+        <img src=${CardInfo.img} class="img-fluid rounded-start" style="height:100%" alt="...">
       </div>
       <div class="col-md-4">
         <div class="card-body">
-          <h5 class="card-title">Autor:${CardInfo.title} </h5>
-          <h3 class="card-title">Titulo:${CardInfo.desc} </h3>
-          <p class="card-text">Body: ${CardInfo.img}</p>
+          <h5 class="card-title">Autor:${CardInfo.data.title} </h5>
+          <h3 class="card-title">desc:${CardInfo.data.desc} </h3>
+          <p class="card-text">Body: ${CardInfo.data.img}</p>
           <button class="btn btn-primary" id="backButton">Regresar</button>
         </div>
       </div>
